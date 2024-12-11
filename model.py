926b5d5fe8ec5031724ef4c44e95ab8e93f51811
Murasame Xu <146193426+YichengXuu@users.noreply.github.com>
2024-12-12 01:25:47 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+
+class LeNet(nn.Module):
+    def __init__(self, input_channels=1, num_classes=10):
+        super().__init__()
+        # TODO
+        
+
+    def forward(self, x):
+        # TODO
